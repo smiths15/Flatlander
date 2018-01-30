8b38906cf87b16d48cd8efbdbb211c0b7e643593
Smith Saintil <smith.saintil@gmail.com>
2018-01-30 06:43:11 -0500

    added panel controller for behavior of panel and tabs

--- app.js
+++ app.js
@@ -5,6 +5,19 @@
     this.products = gems;
   });
 
+//controller for panel//
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    }; 
+
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
   var gems = [
 
     {
